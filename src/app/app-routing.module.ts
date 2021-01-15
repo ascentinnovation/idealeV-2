8b95223b67b4cb882354ab76ab753b881caadc1b
ascentinnovation <dinesh@ascentinnvation.in>
2021-01-15 15:56:38 +0530

    all pages with linking

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {Page1Component} from '../app/page1/page1.component';
+import {NewdemoComponent} from '../app/newdemo/newdemo.component';
+import { Page2Component} from '../app/page2/page2.component';
+import {AproposComponent} from '../app/apropos/apropos.component';
+import {HomeComponent} from '../app/home/home.component';
+import {ConfortComponent} from '../app/confort/confort.component';
+import { SiegeComponent} from '../app/siege/siege.component';
+import { PerformanceComponent } from '../app/performance/performance.component';
+import {HabileteComponent} from '../app/habilete/habilete.component';
+import {SecuriteComponent} from '../app/securite/securite.component';
+import {AideaustationComponent} from '../app/aideaustation/aideaustation.component';
+import {AdialaconduiteComponent} from '../app/adialaconduite/adialaconduite.component';
+import {DesignexterieurComponent} from '../app/designexterieur/designexterieur.component';
+import {EcologieComponent} from '../app/ecologie/ecologie.component';
+import { TransmissionComponent} from '../app/transmission/transmission.component';
+import {EnergieComponent} from '../app/energie/energie.component';
+// import {CategorieComponent}
+import {CategoriedevehiculeComponent} from '../app/categoriedevehicule/categoriedevehicule.component';
+const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path: 'Page1', component:Page1Component},
+  {path: 'demo', component:NewdemoComponent},
+  {path: 'Page2',component:Page2Component},
+  // {path: 'moncompte',component:Page2Component},
+  {path: 'Apropos',component:AproposComponent},
+  // {path: 'maselectionEtSerives',component:Page2Component},
+  //  {path: 'home',component:HomeComponent},
+  // {path: 'seconnecter1',component:Page5Component},
+  // {path: 'seconnecter',component:Page6Component},
+  // {path: 'mandatderecherche',component:Page7Component},
+  {path: 'Securite',component:SecuriteComponent},
+  {path: 'Performance',component:PerformanceComponent},
+  {path: 'Confort',component:ConfortComponent},
+   {path: 'Siege',component:SiegeComponent},
+   {path: 'Habilete',component:HabileteComponent},
+   {path: 'categoriedevehicule',component:CategoriedevehiculeComponent},
+   {path: 'Energie',component:EnergieComponent},
+   {path: 'Transmission',component:TransmissionComponent},
+   {path: 'Aideaustation',component:AideaustationComponent},
+   {path: 'Ecologie',component:EcologieComponent},
+   {path: 'Designexterieur',component:DesignexterieurComponent},
+   {path: 'Adialaconduite',component:AdialaconduiteComponent},
+  //  {path: 'Categorie',component:Page20Component},
+  //  {path: 'asiaaustationinfo',component:Page21Component},
+  //  {path: 'securiteinfo',component:Page22Component},
+  //  {path: 'performanceinfo',component:Page23Component},
+  //  {path: 'ecologieinfo',component:Page24Component},
+  //  {path: 'transmissioninfo',component:Page25Component},
+  //  {path: 'energieinfo',component:Page26Component},
+  //  {path: 'home',component:Page27Component},
+  //  {path: 'validationrecherche',component:Page28Component},
+  //  {path: 'categorie1',component:Page29Component},
+  //  {path: 'resultatdeecherche',component:Page30Component},
+  //  {path: 'detailsfiche1',component:Page31Component},
+  //  {path: 'detailsfichesingle',component:Page32Component},
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+ 
